c343de1c9d0ca6a6ed5cadb3564eff155c72eb57
dhruv-kad1a <work.dhruvkadia@gmail.com>
2024-05-29 17:59:09 +0530

    deva clothing app

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import ShopCategory from './Pages/ShopCategory';
+import Shop from './Pages/Shop'
+import Product from './Pages/Product';
+import LoginSignup from './Pages/LoginSignup'
+import Footer from './Components/Footer/Footer';
+import men_banner from './Components/Assets/banner_mens.png'
+import womwn_banner from './Components/Assets/banner_women.png'
+import kid_banner from './Components/Assets/banner_kids.png'
+import Cart from './Pages/Cart';
+
+function App() {
+  return (
+    <div >
+      <BrowserRouter>
+      <Navbar/>
+      
+      <Routes>
+        <Route path='/' element={<Shop/>}/>
+        <Route path='/mens' element={ <ShopCategory banner={men_banner} category="men"/>}/>
+        <Route path='/womens' element={ <ShopCategory banner={womwn_banner} category="women"/>}/>
+        <Route path='/kids' element={ <ShopCategory banner={kid_banner} category="kid"/>}/>
+
+        <Route path='/product' element={<Product/>}>
+          <Route path=':productId' element={<Product/>}/>
+        </Route>
+
+        <Route path='/carts' element={<Cart/>}/>
+        <Route path='/login' element={<LoginSignup/>}/>
+      </Routes>
+        <Footer/>
+      </BrowserRouter>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/CartItems/CartItems.jsx
@@ -0,0 +1,71 @@
+import React, { useContext } from 'react'
+import './CartItems.css'
+import remove_icon from '../Assets/cart_cross_icon.png'
+import { ShopContext } from '../../Context/ShopContext';
+
+const CartItems = () => {
+
+        const {getTotalCartAmount,all_product,cartItems,removeFromCart}= useContext(ShopContext);
+    
+  return (
+    <div className='cartitems'>
+        <div className="cartitems-formate-main">
+            <p>Products</p>
+            <p>Title</p>
+            <p>Price</p>
+            <p>Quantity</p>
+            <p>Total</p>
+            <p>Remove</p>
+        </div>
+        <hr />
+        {all_product.map((e) => {
+            if(cartItems[e.id]>0){
+                return <div>
+                <div className="cartitems-formate cartitems-formate-main">
+                    <img src={e.image} alt="" className='carticon-product-icon'/>
+                    <p>{e.name}</p>
+                    <p>${e.new_price}</p>
+                    <button className='cartitems-quantity'>{cartItems[e.id]}</button>
+                    <p>${e.new_price*cartItems[e.id]}</p>
+                    <img className='caritems-remove-icon'  src={remove_icon} onClick={() => {removeFromCart(e.id)}} alt="" />
+                </div>
+                <hr />
+            </div>
+            }
+            return null;
+
+        })}
+        <div className="cartitem-down">
+            <div className="cartitem-total">
+                <h1>Cart Totals</h1>
+                <div>
+                    <div className="cartitems-total-item">
+                        <p>Subtotal</p>
+                        <p>${getTotalCartAmount()}</p>
+                    </div>
+                    <hr />
+                    <div className="cartitems-total-item">
+                        <p>Shipping Fee</p>
+                        <p>Free</p>
+                    </div>
+                    <hr />
+                    <div className="cartitems-total-item">
+                        <h3>Total</h3>
+                        <h3>${getTotalCartAmount()}</h3>
+                    </div>
+                </div>
+                <button>PROCEED TO CHECKOUT</button>
+            </div>
+            <div className="cartitems-promocode">
+                <p>If you have a promo code, Enter it here</p>
+                <div className="cartitems-promobox">
+                    <input type="text" placeholder='promo code' />
+                    <button>Submit</button>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default CartItems
